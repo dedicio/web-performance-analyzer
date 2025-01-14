2afcd96bb75d4db923eac5b3cb974d1410b3f232
Dedicio Coelho <dedicio@gmail.com>
2025-01-14 18:44:05 -0300

    feat: add settings

---
+++ app/settings.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use App\Application\Settings\Settings;
+use App\Application\Settings\SettingsInterface;
+use DI\ContainerBuilder;
+
+return function (ContainerBuilder $containerBuilder) {
+    $containerBuilder->addDefinitions([
+        SettingsInterface::class => function () {
+            return new Settings([
+                'google_pagespeed_api_key' => getenv('GOOGLE_PAGESPEED_API_KEY'),
+            ]);
+        }
+    ]);
+};
--- public/index.php
+++ public/index.php
@@ -1,9 +1,16 @@
 <?php
 
-require dirname(__DIR__) . '/vendor/autoload.php';
-
+use DI\ContainerBuilder;
 use Slim\Factory\AppFactory;
 
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+
+$settings = require __DIR__ . '/../app/settings.php';
+$settings($containerBuilder);
+
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
 $routes = require __DIR__ . '/../app/routes.php';
